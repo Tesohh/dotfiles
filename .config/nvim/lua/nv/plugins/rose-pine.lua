bde9a3424887ef0f17819b474582520e659a8b9a
Simone Tesini <simozilla006@gmail.com>
2024-10-09 09:43:47 +0200

    add kitty support to tmux

--- .config/nvim/lua/nv/plugins/rose-pine.lua
+++ .config/nvim/lua/nv/plugins/rose-pine.lua
@@ -1,7 +1,14 @@
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-        vim.cmd([[color rose-pine]])
-    end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	opts = {
+		disable_italics = true,
+		highlight_groups = {
+			Comment = { italic = true },
+		},
+	},
+	config = function(_, opts)
+		require("rose-pine").setup(opts)
+		vim.cmd([[color rose-pine]])
+	end,
 }
