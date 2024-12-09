aedc73350d5611dded45fb62e6b60bd83f684eca
Simone Tesini <simozilla006@gmail.com>
2024-12-09 11:24:08 +0100

    add icons to statusline

--- .config/nvim/lua/nv/niceties/statusline.lua
+++ .config/nvim/lua/nv/niceties/statusline.lua
@@ -1,12 +1,26 @@
-function Oily_filename()
-	local fname = vim.fn.expand("%:.")
+function Statusline_Oily_filename()
+	local fname = vim.fn.expand("%:.") -- Get the current file's name (relative to cwd)
+
+	-- Check for Oil filetype
 	if vim.bo.filetype == "oil" then
-		return fname
+		-- Add custom icon for Oil
+		local oil_icon = " " -- File explorer icon
+		return oil_icon .. " " .. fname
 	elseif vim.bo.buftype == "nofile" then
 		return ""
 	else
-		return fname -- Use the current file's name (relative to cwd)
+		-- Load nvim-web-devicons and fetch the icon
+		local devicons = require("nvim-web-devicons")
+		local extension = vim.fn.fnamemodify(fname, ":e") -- Get file extension
+		local icon = devicons.get_icon(fname, extension, { default = true }) -- Use default if no icon is found
+
+		-- If no icon is found, return only the filename
+		if icon then
+			return icon .. " " .. fname
+		else
+			return fname
+		end
 	end
 end
 
-vim.opt.statusline = "%{v:lua.Oily_filename()}"
+vim.opt.statusline = "%{v:lua.Statusline_Oily_filename()}"
